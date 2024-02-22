8645c1905159e15eae9345f7a8b4244cbda63678
Gabriel <gabriel.ngai1@ucalgary.ca>
2024-02-21 19:06:10 -0700

    transpose matrix method

--- src/main/java/Regression.java
+++ src/main/java/Regression.java
@@ -1,4 +1,6 @@
-public class Regression{
+import java.lang.reflect.Array;
+
+public class Regression {
 
     public static void main(String[] args) {
         System.out.print("Team member: Gabriel Ngai");
@@ -16,4 +18,23 @@ public class Regression{
     private static void test(String[] args) {
 
     }
+
+    /**
+     * Takes in a matrix and transposes it
+     * @param matrixToTranspose is an integer 2D array
+     * @return the transposed matrix
+     */
+    static int[][] transposeMatrix(int[][] matrixToTranspose) {
+        int y = Array.getLength(matrixToTranspose);
+        int x = Array.getLength(matrixToTranspose[0]);
+        System.out.println(y + " " + x);
+        int[][] transposedMatrix = new int[x][y];
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                transposedMatrix[i][j] = matrixToTranspose[j][i];
+            }
+        }
+        return transposedMatrix;
+    }
+
 }
