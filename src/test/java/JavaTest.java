95ad776e62af3a2c70d788aa74746735528cbd12
MatsuriCM <gage.ferguson@ucalgary.ca>
2024-03-01 13:27:06 -0700

    Added some comments to the matrix transposition testing file.

--- src/test/java/JavaTest.java
+++ src/test/java/JavaTest.java
@@ -1,22 +1,21 @@
 import static org.junit.Assert.*;
 import org.junit.Test;
 public class JavaTest {
-    // Tests to ensure the transposition function works as intended.
     @Test
-    public void testSquareTranspose() {
-        Double[][] input = {{1.0, 2.0,3.0},{4.0,5.0,6.0},{7.0,8.0,9.0}};
-        Double[][] expected= {{1.0,4.0,7.0},{2.0,5.0,8.0},{3.0,6.0,9.0}};
-        assertArrayEquals(expected, Regression.transposeMatrix(input));
+    public void testSquareTranspose() { // First tests the transposition of a square matrix.
+        Double[][] input = {{1.0, 2.0,3.0},{4.0,5.0,6.0},{7.0,8.0,9.0}}; // Defines an input 3x3 matrix.
+        Double[][] expected= {{1.0,4.0,7.0},{2.0,5.0,8.0},{3.0,6.0,9.0}}; // Defines the transposed version of the above.
+        assertArrayEquals(expected, Regression.transposeMatrix(input)); // Compares the expected 'test' value matches the one the program calculates.
     }
     @Test
-    public void testRectangleTranspose() {
-        Double[][] input = {{1.0,2.0,3.0},{4.0,5.0,6.0}};
-        Double[][] expected = {{1.0,4.0},{2.0,5.0},{3.0,6.0}};
-        assertArrayEquals(expected,Regression.transposeMatrix(input));
+    public void testRectangleTranspose() { // Tests the transposition of a rectangular matrix.
+        Double[][] input = {{1.0,2.0,3.0},{4.0,5.0,6.0}}; // First defines an input 3x2 matrix.
+        Double[][] expected = {{1.0,4.0},{2.0,5.0},{3.0,6.0}}; // Then the manual transposition of the matrix is listed as a 2x3 matrix.
+        assertArrayEquals(expected,Regression.transposeMatrix(input)); // The expected, manual value is then compared to the calculated one.
     }
     @Test
-    public void testEmptyTranspose() {
+    public void testEmptyTranspose() { // Lastly, tests the transposition of a completely empty matrix.
         Double[][] input = {};
-        assertEquals(0, Regression.transposeMatrix(input).length);
+        assertEquals(0, Regression.transposeMatrix(input).length); // Makes sure that the end result of the matrix is still empty.
     }
 }
